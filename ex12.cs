794877adb1d6fa73a3f7f9b851f18c4111359475
mariananevesz <nevespereiramariana9@gmail.com>
2026-03-20 17:48:19 -0300

    Commit 15

--- ex12.cs
+++ ex12.cs
@@ -1,9 +1,9 @@
-/*using System;
+using System;
 
 namespace variaveisEstruturda{
-   class ex10{
+   class ex12{
         static void Main(string[] args){
-        int n, media, soma;
+        int n, soma;
         soma = 0;
      
             Console.WriteLine("Digite o tamanho do vetor: "); 
@@ -11,17 +11,15 @@ namespace variaveisEstruturda{
             int[] vetor = new int[n]; 
 
             for(int i = 0; i < vetor.Length; i++){ 
-                Console.WriteLine("Digite uma temperatura: ");
+                Console.WriteLine("Digite um número: ");
                 vetor[i] = Convert.ToInt32(Console.ReadLine());
             }
 
             for (int i = 0; i<vetor.Length; i++){
-                    soma = soma + vetor[i];
+                    soma = (vetor[i]*vetor[i]) + (vetor[i]*vetor[i]) ;
                     Console.WriteLine("Vetor["+i+"]"+vetor[i]);
                 }
-            media = soma/n;
-
-            Console.WriteLine("A media das temper é: "+media);
+             Console.WriteLine("A soma dos quadrados dos números é: "+soma);
         }    
     }
-}*/
+}
